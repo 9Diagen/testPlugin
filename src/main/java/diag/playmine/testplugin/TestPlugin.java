c00649fdb34ba4f5ae3e0d1b65db222f942aa759
9Diagen <9diagen@gmail.com>
2023-11-15 16:50:24 +0500

    First Comm

--- src/main/java/diag/playmine/testplugin/TestPlugin.java
+++ src/main/java/diag/playmine/testplugin/TestPlugin.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin {
 
+    public static String as = "1234";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
